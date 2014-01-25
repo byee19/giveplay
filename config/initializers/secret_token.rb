c4fa17fea6ba7733c96cb6914fcffd58387071ce
Kirk McPherson <kirk_mcpherson@yahoo.com>
2014-01-25 15:40:47 -0500

    giveplay

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giveplay::Application.config.secret_token = 'e68dd97e87077a390026d488ef889a02f5e4fc7ab2a5045ac5c03bdacf6d70a6375b61777fd8cc9c94c9ffe1d1683ae581113142a4f42eb7fcf57f491ae1c981'
